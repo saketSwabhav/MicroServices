2135c45455e6b68178907ca52623e9433f1cebf4
SAket MIshra <93260793+saketSwabhav@users.noreply.github.com>
2022-03-30 11:15:50 +0530

    init

---
+++ ui/src/DisplayComment.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+
+
+export default ({comments})=>{
+
+    /*const [comments,updateComments] = useState([])
+  
+     const loadComments = async () =>{
+       const resp  = await axios.get(`http://localhost:4002/api/v1/blog/post/${postId}/comment`)
+       updateComments(resp.data)
+
+     }
+    useEffect(()=>{
+        loadComments();
+    },[])*/
+
+     console.log(comments)
+   const liOfComments = comments.map(c=>{
+       return (
+           <li key={c.commentId}>
+              {c.message}
+           </li>
+       );
+   })
+
+  return (
+     <ol>
+          {liOfComments}
+     </ol>
+  );
+
+}
